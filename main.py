5126a366514bf35d962e303ae487247eb7535edd
VladimirMaximov <vladimirmaximov2004@mail.ru>
2023-05-02 22:03:45 +0300

    Create .gitignore file and start page of app

---
+++ main.py
@@ -0,0 +1,81 @@
+import tkinter as tk
+
+
+class MainFrame(tk.Frame):
+
+    def __init__(self, parent: tk.Tk):
+        tk.Frame.__init__(self, parent, background="#FFFFFF")
+        self.parent = parent
+        self.set_window()
+        self.create_elements()
+        self.pack(expand=1)
+        # Function for fixing the frame size - pack_propagate(False)
+
+    def create_elements(self):
+        frame_for_label = tk.Frame(self,
+                                   background="#FFFFFF",
+                                   width=800,
+                                   height=100,
+                                   border=10
+                                   )
+        frame_for_label.pack(expand=1)
+
+        frame_for_buttons = tk.Frame(self,
+                                     background="#FFFFFF",
+                                     width=700,
+                                     height=100,
+                                     border=10
+                                     )
+        frame_for_buttons.pack(expand=1)
+
+        label = tk.Label(frame_for_label, text="Шашечный турнирный менеджер",
+                         font=("Times New Roman", 40),
+                         background="#FFFFFF"
+                         )
+        label.pack(pady=(20, 0))
+
+        button_new_tournament = tk.Button(frame_for_buttons, text="Новый турнир",
+                                          font=("Times New Roman", 14),
+                                          background="#FFFFFF",
+                                          width=20,
+                                          height=2,
+                                          relief="solid",
+                                          activebackground="#FFFFFF"
+                                          )
+        button_new_tournament.pack(side="left", padx=(90, 40), pady=(0, 30))
+
+        button_new_tournament = tk.Button(frame_for_buttons, text="Загрузить турнир",
+                                          font=("Times New Roman", 14),
+                                          background="#FFFFFF",
+                                          width=20,
+                                          height=2,
+                                          relief="solid",
+                                          activebackground="#FFFFFF"
+                                          )
+
+        button_new_tournament.pack(side="right", padx=(40, 90), pady=(0, 30))
+
+    def set_window(self):
+
+        width = 1000
+        height = 600
+        self.parent.title("Турнирный менеджер")
+        self.parent.config(bg="#FFFFFF")
+
+        screen_width = self.parent.winfo_screenwidth()
+        screen_height = self.parent.winfo_screenheight()
+
+        x = (screen_width - width) / 2
+        y = (screen_height - height) / 2
+
+        self.parent.geometry(f"{width}x{height}+{int(x)}+{int(y)}")
+
+
+def main():
+    window = tk.Tk()
+    MainFrame(window)
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
