8d93a7ec3cbb6b5c094e096c199b12c9e4d60bbb
VladimirMaximov <vladimirmaximov2004@mail.ru>
2024-01-22 15:29:35 +0300

    rebuild from grid to pack and add 'edit' botton

--- src/participant_entry_page/participants_frame.py
+++ src/participant_entry_page/participants_frame.py
@@ -22,58 +22,99 @@ class ParticipantsFrame(tk.Frame):
         pass
 
     def create_elements(self):
-        # fft - frame for text
-        fft = tk.Frame(self,
-                       background="#FFFFFF",
-                       width=1000,
-                       height=60
-                       )
-        fft.pack(expand=1, fill="both")
-        # ffp - frame for participants
-        ffp = tk.Frame(self,
-                       background="#FFFFFF",
-                       width=1000,
-                       height=400
-                       )
-        ffp.pack(expand=1, fill="both")
-        # ffb - frame for buttons
-        ffb = tk.Frame(self,
-                       background="#FFFFFF",
-                       width=1000,
-                       height=100
-                       )
-        ffb.pack(expand=1, fill="both")
+        frame_for_label = tk.Frame(self,
+                                   background="#FFFFFF",
+                                   width=1000,
+                                   height=50
+                                   )
 
-        label0 = tk.Label(fft, text="Добавление участников",
+        frame_for_label.pack(expand=1, fill="both")
+        frame_for_entry = tk.Frame(self,
+                                   background="#FFFFFF",
+                                   width=1000,
+                                   height=50
+                                   )
+
+        frame_for_entry.pack(expand=1, fill="both")
+        frame_for_listbox = tk.Frame(self,
+                                  background="#FFFFFF",
+                                  width=1000,
+                                  height=400
+                                  )
+
+        frame_for_listbox.pack(expand=1, fill="both")
+        frame_for_buttons = tk.Frame(self,
+                                     background="#FFFFFF",
+                                     width=1000,
+                                     height=100
+                                     )
+        frame_for_buttons.pack(expand=1, fill="both")
+
+        label0 = tk.Label(frame_for_label, text="Добавление участников",
                           font=("Times New Roman", 36),
                           background="#FFFFFF",
                           width=36)
-        label0.grid(row=0, column=0, pady=5, padx=10, columnspan=2)
+        label0.pack()
 
-        label1 = tk.Label(fft,
+        label1 = tk.Label(frame_for_entry,
                           text="Введите ФИО участника: ",
                           font=("Times New Roman", 14),
                           background="#FFFFFF",
                           width=23,
                           anchor="w"
                           )
-        label1.grid(row=1, column=0, sticky="w", pady=5, padx=(7, 5))
+        label1.pack(side="left")
 
         def add_player(event=None):
+
+            # Если поле не пустое, тогда добавляем нового игрока
             if entry1.get() != "":
+                # Достаем имя игрока
                 new_player = entry1.get()
-                self.tn.players.append(tart.Player(len(self.tn.players), new_player))
+
+                # Добавляем игрока в список игроков
+                self.tn.players.append(tart.Player(len(self.tn.players) + 1, new_player))
+
+                # Обновляем список игроков в виджете Text
                 players_var.set([f" {i + 1}. " + self.tn.players[i].name for i in range(len(self.tn.players))])
+
+                # Очищаем поле ввода имени
                 entry1.delete(0, tk.END)
 
         def delete_player():
+
+            # Получаем кортеж индексов выделенных строк
             selection = listbox.curselection()
+
+            # Если выделена хотя бы одна строка
             if len(selection) != 0:
+                # Удаляем первую строку, которая была выделена
                 self.tn.players.pop(selection[0])
+
+                # Обновляем список игроков в виджете Text
                 players_var.set([f" {i + 1}. " + self.tn.players[i].name for i in range(len(self.tn.players))])
+
+                # Снимаем выделение с полей
                 listbox.selection_clear(0, tk.END)
 
+        def edit_player():
 
+            # Получаем кортеж индексов выделенных строк
+            selection = listbox.curselection()
+
+            # Если выделена хотя бы одна строка
+            if len(selection) != 0:
+
+                # Изменяем первую строку, которая была выделена
+                self.tn.players[selection[0]] = tart.Player(selection[0], entry1.get())
+
+                # Обновляем список игроков в виджете Text
+                players_var.set([f" {i + 1}. " + self.tn.players[i].name for i in range(len(self.tn.players))])
+
+                # Снимаем выделение с полей
+                listbox.selection_clear(0, tk.END)
+
+        # Функция, которая задает в поле ввода имя то значение, которое выделено
         def set_entry1(event):
             selection = listbox.curselection()
             if len(selection) != 0:
@@ -81,19 +122,20 @@ class ParticipantsFrame(tk.Frame):
                 entry1.delete(0, tk.END)
                 entry1.insert(0, " ".join(set_player.split()[1:]))
 
+        # Переменная поля entry1
         player = tk.StringVar()
-        entry1 = tk.Entry(fft,
+        entry1 = tk.Entry(frame_for_entry,
                           font=("Times New Roman", 14),
                           background="#E8E8E8",
                           relief="flat",
                           width=81,
                           textvariable=player
                           )
-        entry1.grid(row=1, column=1, sticky="w", pady=5, padx=(5, 5))
+        entry1.pack(side="left")
         entry1.bind("<Return>", add_player)
 
         players_var = tk.Variable(value=[f" {i + 1}. " + self.tn.players[i].name for i in range(len(self.tn.players))])
-        listbox = tk.Listbox(ffp,
+        listbox = tk.Listbox(frame_for_listbox,
                              relief="solid",
                              border=2,
                              font=("Times New Roman", 14),
@@ -104,55 +146,64 @@ class ParticipantsFrame(tk.Frame):
         listbox.pack(anchor="sw", side="left", fill="both", padx=5, pady=5)
         listbox.bind("<<ListboxSelect>>", set_entry1)
 
-        scrollbar = tk.Scrollbar(ffp, orient="vertical", command=listbox.yview)
+        scrollbar = tk.Scrollbar(frame_for_listbox, orient="vertical", command=listbox.yview)
         scrollbar.pack(anchor="sw", side="right", fill="y", padx=(5, 11), pady=5)
         listbox["yscrollcommand"] = scrollbar.set
 
-        button_back = tk.Button(ffb, text="Назад",
+        button_back = tk.Button(frame_for_buttons, text="Назад",
                                 font=("Times New Roman", 14),
                                 background="#FFFFFF",
-                                width=20,
+                                width=18,
                                 height=2,
                                 relief="solid",
                                 activebackground="#FFFFFF",
                                 command=self.create_parameters_frame
                                 )
-        button_back.grid(row=0, column=0, sticky="W", padx=(5, 25), pady=(10, 0))
+        button_back.pack(side="left", padx=5, pady=10)
 
-        button_delete = tk.Button(ffb,
+        button_delete = tk.Button(frame_for_buttons,
                                   text="Удалить",
                                   font=("Times New Roman", 14),
                                   background="#FFFFFF",
-                                  width=20,
+                                  width=18,
                                   height=2,
                                   relief="solid",
                                   activebackground="#FFFFFF",
                                   command=delete_player
                                   )
-        button_delete.grid(row=0, column=1, sticky="W", padx=25, pady=(10, 0))
+        button_delete.pack(side="left", padx=5, pady=10)
 
-        button_add = tk.Button(ffb,
+        button_add = tk.Button(frame_for_buttons,
+                               text="Редактировать",
+                               font=("Times New Roman", 14),
+                               background="#FFFFFF",
+                               width=18,
+                               height=2,
+                               relief="solid",
+                               activebackground="#FFFFFF",
+                               command=edit_player
+                               )
+        button_add.pack(side="left", padx=5, pady=10)
+
+        button_add = tk.Button(frame_for_buttons,
                                text="Добавить",
                                font=("Times New Roman", 14),
                                background="#FFFFFF",
-                               width=20,
+                               width=18,
                                height=2,
                                relief="solid",
                                activebackground="#FFFFFF",
                                command=add_player
                                )
-        button_add.grid(row=0, column=2, sticky="W", padx=25, pady=(10, 0))
+        button_add.pack(side="left", padx=5, pady=10)
 
-        text = "Далее"
-        command = self.create_tours_frame
-
-        button_next = tk.Button(ffb, text=text,
+        button_next = tk.Button(frame_for_buttons, text="Далее",
                                 font=("Times New Roman", 14),
                                 background="#FFFFFF",
-                                width=20,
+                                width=18,
                                 height=2,
                                 relief="solid",
                                 activebackground="#FFFFFF",
-                                command=command
+                                command=self.create_tours_frame
                                 )
-        button_next.grid(row=0, column=3, sticky="W", padx=(20, 5), pady=(10, 0))
+        button_next.pack(side="left", padx=5, pady=10)
