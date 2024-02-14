262143cfb64e0282fcc1f79b36f2a82498105aac
VladimirMaximov <vladimirmaximov2004@mail.ru>
2024-02-14 23:24:57 +0300

    refactor systems of coefficients

--- src/tournament/player.py
+++ src/tournament/player.py
@@ -1,9 +1,6 @@
 class Player:
 
-    def __init__(self, number, name, list_of_opponents: list = None, number_of_points=0,
-                 number_of_wins=0, schmullan_coefficient=0, buchholz_coefficient=0,
-                 median_solkoff_coefficient=0, truncated_solkoff_coefficient_1=0,
-                 truncated_solkoff_coefficient_2=0, place=None):
+    def __init__(self, number, name, list_of_opponents: list = None, number_of_points=0, place=None):
         self.number = number  # Порядковый номер
         self.name = name
 
@@ -15,12 +12,6 @@ class Player:
             self.list_of_opponents = list_of_opponents
 
         self.number_of_points = number_of_points
-        self.number_of_wins = number_of_wins
-        self.schmullan_coefficient = schmullan_coefficient
-        self.buchholz_coefficient = buchholz_coefficient
-        self.median_solkoff_coefficient = median_solkoff_coefficient
-        self.truncated_solkoff_coefficient_1 = truncated_solkoff_coefficient_1
-        self.truncated_solkoff_coefficient_2 = truncated_solkoff_coefficient_2
         self.place = place
 
     def __eq__(self, other):
@@ -35,24 +26,25 @@ class Player:
         else:
             return 0
 
+    systems_of_coefficients = ["Система коэффициентов Шмульяна",
+                               "Cистема коэффициентов Бухгольца",
+                               "Медианный коэффициент Солкофа",
+                               "Короткий усеченный коэффициент Солкофа",
+                               "Полный усеченный коэффициент Солкофа"]
+
     def get_coefficient(self, name_of_coefficient):
         if name_of_coefficient == "Наибольшее число побед":
-            return self.number_of_wins
+            return self.get_count_of_wins()
         elif name_of_coefficient == "Коэффициент Шмульяна":
-            self.set_schmullan_coefficient()
-            return self.schmullan_coefficient
+            return self.get_schmullan_coefficient()
         elif name_of_coefficient == "Коэффициент Бухгольца":
-            self.set_buchholz_coefficient()
-            return self.buchholz_coefficient
+            return self.get_buchholz_coefficient()
         elif name_of_coefficient == "Медианный коэффициент Солкофа":
-            self.set_median_solkoff_coefficient()
-            return self.median_solkoff_coefficient
-        elif name_of_coefficient == "Полный усеченный коэффициент Солкофа (без 1 худшего результата)":
-            self.set_full_truncated_solkoff_coefficient_without_one_worst_result()
-            return self.truncated_solkoff_coefficient_1
-        elif name_of_coefficient == "Полный усеченный коэффициент Солкофа (без 2 худших результатов)":
-            self.set_full_truncated_solkoff_coefficient_without_two_worst_result()
-            return self.truncated_solkoff_coefficient_2
+            return self.get_median_solkoff_coefficient()
+        elif name_of_coefficient == "Полный усеченный коэффициент Солкофа":
+            return self.get_full_truncated_solkoff_coefficient()
+        elif name_of_coefficient == "Короткий усеченный коэффициент Солкофа":
+            return self.get_short_truncated_solkoff_coefficient()
         else:
             return 0
 
@@ -67,80 +59,53 @@ class Player:
         else:
             return self.get_coefficient(priority)
 
-    def set_schmullan_coefficient(self):
+    def get_count_of_wins(self):
+        res = 0
+        for opponent, result, color in self.list_of_opponents:
+            if result == 2:
+                res += 1
+        return res
+
+    def get_schmullan_coefficient(self):
         winner_points = 0
         loser_points = 0
 
-        list_of_opponents = self.list_of_opponents
-
-        for opponent, result, color in list_of_opponents:
+        for opponent, result, color in self.list_of_opponents:
             if result == 2:
                 winner_points += opponent.number_of_points
             if result == 0:
                 loser_points += opponent.number_of_points
 
-        self.schmullan_coefficient = abs(winner_points - loser_points)
+        return winner_points - loser_points
 
-    def set_buchholz_coefficient(self):
-        result = 0
+    def get_buchholz_coefficient(self):
+        res = 0
 
-        for opponent in self.list_of_opponents:
-            result += opponent.number_of_points
+        for opponent, result, color in self.list_of_opponents:
+            res += opponent.number_of_points
 
-        self.buchholz_coefficient = result
+        return res
 
-    def set_median_solkoff_coefficient(self):
-        result = 0
+    def get_median_solkoff_coefficient(self):
+        res_of_opponents = []
+        for opponent, result, color in self.list_of_opponents:
+            res_of_opponents.append(opponent.number_of_points)
 
-        minimal_result = 10000000
-        maximal_result = -1
+        res_of_opponents.sort()
 
-        for opponent in self.list_of_opponents:
-            result += opponent.number_of_points
-
-            if opponent.number_of_points < minimal_result:
-                minimal_result = opponent.number_of_points
-
-            if opponent.number_of_points > maximal_result:
-                maximal_result = opponent.number_of_points
-
-        if minimal_result != 10000000:
-            return result - minimal_result - maximal_result
-        else:
+        if len(res_of_opponents) <= 2:
             return 0
-
-    def set_full_truncated_solkoff_coefficient_without_one_worst_result(self):
-        result = 0
-
-        minimal_result = 10000000
-
-        for opponent in self.list_of_opponents:
-            result += opponent.number_of_points
-
-            if opponent.number_of_points < minimal_result:
-                minimal_result = opponent.number_of_points
-
-        if result != 10000000:
-            return result - minimal_result
         else:
-            return 0
+            return sum(res_of_opponents[1:-1])
 
-    def set_full_truncated_solkoff_coefficient_without_two_worst_result(self):
-        result = 0
+    def get_short_truncated_solkoff_coefficient(self):
+        return self.get_full_truncated_solkoff_coefficient()[0]
 
-        minimal_result = 100000000
-        minimal_result_2 = 100000000
-        for opponent in self.list_of_opponents:
-            result += opponent.number_of_points
+    def get_full_truncated_solkoff_coefficient(self):
 
-            if opponent.number_of_points < minimal_result:
-                minimal_result = opponent.number_of_points
-            elif opponent.number_of_points < minimal_result_2:
-                minimal_result_2 = opponent.number_of_points
+        res_of_opponents = []
+        for opponent, result, color in self.list_of_opponents:
+            res_of_opponents.append(opponent.number_of_points)
 
-        if minimal_result != 100000000 and minimal_result_2 != 100000000:
-            return result - minimal_result - minimal_result_2
-        elif minimal_result != 100000000:
-            return result - minimal_result
-        else:
-            return 0
+        res_of_opponents.sort()
+        return [sum(res_of_opponents[i::]) for i in range(1, len(res_of_opponents))]
